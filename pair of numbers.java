43b10e5559ab0caead37c878885ac7597d339985
Bilal Butt <62094039+Bilalbut@users.noreply.github.com>
2021-03-26 16:03:36 +0500

    Pair of numbers adds upto 20
    Write a program that reads the numbers a, b, c and checks if there's a pair of them that adds up to exactly 20.
    
    The program must output true or false

---
+++ pair of numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(a + b == 20 || b + c == 20 || a + c == 20);
+    }
+}
